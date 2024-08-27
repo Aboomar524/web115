5d1ee24ba93fc7cd6482de81264750c3bc8de38f
Ahmad <111832798+Aboomar524@users.noreply.github.com>
2024-08-27 15:55:47 -0400

    aa

--- scripts/validation.js
+++ scripts/validation.js
@@ -1,107 +1,65 @@
-// generate navigation bar using 3 arguments
-function navBar(nav, linkNames, linkHREFs) {
-    for (let i = 0; i < linkNames.length; i++) {
-        let link = document.createElement("a");
-        link.innerHTML = linkNames[i];
-        link.href = linkHREFs[i];
-        nav.append(link);
-    }
-}
-
-function addHeader() {
-    // create header element at top of body
-    const header = document.createElement("header");
-    document.body.prepend(header);
-
-    // add name and site heading
-    const h1 = document.createElement("h1");
-    const siteName = "Syrian Sweets";
-    h1.innerHTML = siteName;
-    header.append(h1);
-
-    // add banner with slogan
-    const sloganBanner = document.createElement("h3");
-    sloganBanner.innerHTML = "<em>The Best Sweets from the Heart of Syria</em>";
-    header.append(sloganBanner);
-
-    // add nav container
-    const nav = document.createElement("nav");
-    header.append(nav);
-
-    // add first link as a logo
-    const logoLink = document.createElement("a");
-    logoLink.href = "index.html";
-    logoLink.classList.add("logo");
-    const logo = new Image();
-    logo.src = "images/logo.png"; // Make sure the logo image is available
-    logoLink.appendChild(logo);
-    nav.appendChild(logoLink);
-
-    // add navigation links
-    fetch('scripts/navLinks.json')
-        .then(res => { return res.json() })
-        .then(data => {
-            const headerLinks = data.headerLinks;
-            navBar(nav, headerLinks.names, headerLinks.hrefs);
-        })
-        .catch(error => console.log(error));
-
-    // add hr line
-    const line = document.createElement("hr");
-    header.append(line);
-}
-
-function addFooter() {
-    // create footer element at end of body
-    const footer = document.createElement("footer");
-    document.body.append(footer);
-
-    // add hr line
-    const line = document.createElement("hr");
-    footer.append(line);
-
-    // add nav container
-    const nav = document.createElement("nav");
-    footer.append(nav);
-
-    // add navigation links
-    fetch('scripts/navLinks.json')
-        .then(res => { return res.json() })
-        .then(data => {
-            const footerLinks = data.footerLinks;
-            navBar(nav, footerLinks.names, footerLinks.hrefs);
-        })
-        .catch(error => console.log(error));
-
-    // add signature
-    const sigContent = `Designed by <strong>&copy;Ahmad Alhomair</strong>`;
-    const signature = document.createElement("p");
-    signature.innerHTML = sigContent;
-    footer.append(signature);
-
-    // add validation block
-    const validationBlock = document.createElement("div");
-    validationBlock.classList.add("validate");
-    footer.append(validationBlock);
-
-    fetch('scripts/navLinks.json')
-        .then(res => { return res.json() })
-        .then(data => {
-            const validationLinks = data.validationLinks;
-            for (let i = 0; i < validationLinks.names.length; i++) {
-                let link = document.createElement("a");
-                link.innerHTML = validationLinks.names[i];
-                link.href = validationLinks.hrefs[i];
-                link.setAttribute("id", validationLinks.ids[i]);
-                validationBlock.append(link);
+/*! HTMLInclude v1.1.1 | MIT License | github.com/paul-browne/HTMLInclude */
+!(function (l, d) {
+    l.HTMLInclude ||
+        (l.HTMLInclude = function () {
+            function r(t, e) {
+                return t.getBoundingClientRect().top <= +e + l.innerHeight;
             }
-            // update validation links based on the current page
-            document.getElementById("validation_link_html").setAttribute("href", "https://validator.w3.org/check?uri=" + location.href);
-            document.getElementById("validation_link_css").setAttribute("href", "https://jigsaw.w3.org/css-validator/validator?uri=" + location.href);
-        })
-        .catch(error => console.log(error));
-}
+            function a(t, e) {
+                var o = new XMLHttpRequest();
+                (o.onreadystatechange = function () {
+                    4 === o.readyState &&
+                        200 === o.status &&
+                        e.forEach(function (t) {
+                            var e = t.getAttribute("data-replace"),
+                                n = o.responseText;
+                            e &&
+                                e.split(",").forEach(function (t) {
+                                    var e = t.trim().split(":");
+                                    n = n.replace(new RegExp(e[0], "g"), e[1]);
+                                }),
+                                (t.outerHTML = n);
+                            for (
+                                var r = new DOMParser()
+                                    .parseFromString(n, "text/html")
+                                    .querySelectorAll("SCRIPT"),
+                                a = 0,
+                                i = r.length;
+                                a < i;
 
-window.onload =
-    addHeader();
-addFooter();
+                            ) {
+                                var c = d.createElement("SCRIPT");
+                                r[a].src ? (c.src = r[a].src) : (c.innerHTML = r[a].innerHTML),
+                                    d.head.appendChild(c),
+                                    a++;
+                            }
+                        });
+                }),
+                    o.open("GET", t, !0),
+                    o.send();
+            }
+            function t(e, n) {
+                l.addEventListener("scroll", function t() {
+                    r(e, n) &&
+                        (l.removeEventListener("scroll", t),
+                            a(e.getAttribute("data-include"), [e]));
+                });
+            }
+            for (
+                var e = {},
+                n = d.querySelectorAll("[data-include]:not([data-in])"),
+                i = n.length;
+                i--;
+
+            ) {
+                var c = n[i].getAttribute("data-include"),
+                    o = n[i].getAttribute("data-lazy");
+                n[i].setAttribute("data-in", ""),
+                    !o || (o && r(n[i], o))
+                        ? ((e[c] = e[c] || []), e[c].push(n[i]))
+                        : t(n[i], o);
+            }
+            for (var u in e) a(u, e[u]);
+        }),
+        l.HTMLInclude();
+})(window, document);
