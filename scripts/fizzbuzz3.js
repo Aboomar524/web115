28d6419074b628f0a94eda97f663bd47dcbdf29a
Ahmad <111832798+Aboomar524@users.noreply.github.com>
2024-11-26 10:40:38 -0500

    s

--- scripts/fizzbuzz3.js
+++ scripts/fizzbuzz3.js
@@ -1,35 +1,27 @@
 const form = document.getElementById("name-form");
 const fizzBuzzBang = [{ 3: "yum!" }, { 5: "tasty!" }, { 7: "BANG!" }];
-
 const checkDivision = (firstDivisor, secondDivisor) => firstDivisor % secondDivisor === 0;
-
 form.addEventListener("submit", function (event) {
     event.preventDefault();
-
     const firstName = document.getElementById("first-name").value;
     const middleInitial = document.getElementById("middle-initial").value;
     const lastName = document.getElementById("last-name").value;
     const greeting = document.getElementById("greeting");
     const loopList = document.getElementById("loop-list");
-
     if (!firstName) {
         alert("Please enter your first name.");
         document.getElementById("first-name").focus();
         return;
     }
-
     if (!lastName) {
         alert("Please enter your last name.");
         document.getElementById("last-name").focus();
         return;
     }
-
     greeting.textContent = `Welcome to Sweet Syria, ${firstName} ${middleInitial}${middleInitial ? "." : ""} ${lastName}!`
         .replace(/\s+/g, " ")
         .trim();
-
     loopList.innerHTML = "";
-
     let count;
     do {
         count = prompt(`How high do you like to count, ${firstName}?`);
@@ -59,7 +51,7 @@ form.addEventListener("submit", function (event) {
             }
         }
 
-        listItem.textContent = result ? `${x}: ${result.trim()}` : x;
+        listItem.textContent = result; //? ` ${result.trim()}` : x;
         loopList.appendChild(listItem);
     }
 });
